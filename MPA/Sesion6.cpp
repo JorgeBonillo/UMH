0005fa31aa55914cec37771c64a5a3f1c8cda938
Jorge Sánchez Bonillo <jorgebonillo1998@hotmail.es>
2018-04-25 09:48:48 +0200

    25-04.18

--- MPA/Sesion6.cpp
+++ MPA/Sesion6.cpp
@@ -90,59 +90,7 @@ void Mochila(int n, float m, float *p, float *v, float *x, int k, float *xopt, f
 	cin >> M;
 
 
-	pesos[1] = 0.450;
-	pesos[2] = 0.300;
-	pesos[3] = 0.200;
-	pesos[4] = 0.430;
-	pesos[5] = 0.300;
-	pesos[6] = 0.145;
-	pesos[7] = 0.170;
-	pesos[8] = 0.400;
-	pesos[9] = 1.100;
-	pesos[10] = 0.750;
-	pesos[11] = 0.280;
-	pesos[12] = 1.000;
-	pesos[13] = 0.150;
-	pesos[14] = 0.350;
-	pesos[15] = 0.350;
-	pesos[16] = 0.400;
-	pesos[17] = 0.500;
-	pesos[18] = 0.200;
-	pesos[19] = 0.300;
-	pesos[20] = 0.600;
-	pesos[21] = 0.600;
-	pesos[22] = 0.750;
-	pesos[23] = 0.400;
-	pesos[24] = 1.200;
-	pesos[25] = 1.100;
 
-	valores[1] = 5;
-	valores[2] = 7;
-	valores[3] = 7;
-	valores[4] = 10;
-	valores[5] = 10;
-	valores[6] = 6;
-	valores[7] = 9;
-	valores[8] = 9;
-	valores[9] = 7;
-	valores[10] = 5;
-	valores[11] = 7;
-	valores[12] = 9;
-	valores[13] = 10;
-	valores[14] = 8;
-	valores[15] = 9.5;
-	valores[16] = 6;
-	valores[17] = 9;
-	valores[18] = 10;
-	valores[19] = 10;
-	valores[20] = 6.5;
-	valores[21] = 7.5;
-	valores[22] = 10;
-	valores[23] = 8;
-	valores[24] = 9;
-	valores[25] = 8;
-
-  /*
 	 cout << "Pesos" << endl;
 	 for(int i=1; i<=n; i++){
 
@@ -158,7 +106,6 @@ void Mochila(int n, float m, float *p, float *v, float *x, int k, float *xopt, f
 	 cin >> valores[i];
 
 	 }
-*/
 
 	cout << endl;
 	for (int i = 1; i <= n; i++)
